3da838c6ded9d64af90bfec247e13d1aa7e3fd89
Kari O'Neal <triadfirewood@gmail.com>
2018-07-21 11:22:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,14 @@
 # Your code goes here!
 class Dog 
     def initialize (name)
-      @name=name
+      @name = name
     end
     
-    def name=(new_name)
-      @name = new_name
+    def name=(name)
+      @name = name
     end
     
     def name
       @name
     end
-    
-    
- 
-  puts "woof!"
-    
 end
